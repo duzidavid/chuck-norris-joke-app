075b6d218a062a7745ed65f03edb8d6471e5cbfc
duzidavid <david.duzi@teliacompany.com>
2020-01-13 13:23:16 +0100

    Spring XML configuration bean example.

--- src/main/java/cz/duzi/chucknorrisjokeapp/ChuckNorrisJokeAppApplication.java
+++ src/main/java/cz/duzi/chucknorrisjokeapp/ChuckNorrisJokeAppApplication.java
@@ -4,8 +4,10 @@ import cz.duzi.chucknorrisjokeapp.services.JokeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokeAppApplication {
 
     public static void main(String[] args) {
